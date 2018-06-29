1af248f69acb9898208e9a8522b5104d5aa51fb5
Martin Allimonier <allimonier.martin@gmail.com>
2018-06-29 09:15:25 +0200

    mise en forme du formulaire de création d'un kill

--- webapp/controllers/killController.js
+++ webapp/controllers/killController.js
@@ -5,7 +5,16 @@ var kill = mongoose.model('Kill');
 
 module.exports.addKill = function(req, res){
     let new_kill = new kill(req.body);
-    console.log(req.body);
+    if(new_kill.isEmpty = "on"){
+        new_kill.isEmpty = true;
+    }else{
+        new_kill.isEmpty = false;
+    }
+    if(new_kill.isLegend = "on"){
+        new_kill.isLegend = true;
+    }else{
+        new_kill.isLegend = false;
+    }
     new_kill.save(function(err, kill){
         if(err){
             res.send(err);
