9fe1c4becad8e21f57203d805eaf06d535e0cd73
Martin Allimonier <allimonier.martin@gmail.com>
2018-07-03 20:56:57 +0200

    style

--- webapp/controllers/killController.js
+++ webapp/controllers/killController.js
@@ -5,6 +5,16 @@ var kill = mongoose.model('Kill');
 
 module.exports.addKill = function(req, res){
     let new_kill = new kill(req.body);
+    if(req.body.isLegend == "on"){
+        new_kill.isLegend = true;
+    }else{
+        new_kill.isLegend = false;
+    }
+    if(req.body.isEmpty == "on"){
+        new_kill.isEmpty = true;
+    }else{
+        new_kill.isEmpty = false;
+    }
     console.log(req.body);
     new_kill.save(function(err, kill){
         if(err){
